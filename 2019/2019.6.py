6c75aee3d730bf85b62b9f4f8267fe879d30f464
Boris Kashentsev <boris.kashentsev@gmail.com>
2019-12-10 12:44:30 +0200

    Day 6 of 2019. First part

---
+++ 2019/2019.6.py
@@ -0,0 +1,54 @@
+class node:
+    parent = False
+    name = ""
+    depth = 0
+    children = []
+    def introduction(self):
+        return "Name: " + self.name + "\n  Depth: " + str(self.depth) + "\n  Parent: " + str(self.parent) + "\n  # of children: " + str(len(self.children))
+
+def calucateDepth(element, dictionary, depth):
+    if element.parent != False :
+        element.depth = dictionary[element.parent].depth + 1
+        depth += element.depth
+    
+    for child in element.children:
+        depth = calucateDepth(child, dictionary, depth)
+    return depth
+
+filename = "2019.6.test1.input"
+# filename = "2019.6.input"
+
+f = open(filename, "r")
+
+inpt = f.read().split("\n")
+
+dictionary = {}
+
+for line in inpt:
+    parts = line.split(")")
+    if parts[1] not in dictionary:
+        newNode = node()
+        newNode.name = parts[1]
+        newNode.parent = parts[0]
+        newNode.children = []
+        dictionary[parts[1]] = newNode
+    else:
+        dictionary[parts[1]].parent = parts[0]
+
+    if parts[0] not in dictionary:
+        newNode = node()
+        newNode.name = parts[0]
+        newNode.children = []
+        dictionary[parts[0]] = newNode
+    dictionary[parts[0]].children.append(dictionary[parts[1]])
+
+root = False
+
+for value in dictionary.values():
+    if not value.parent:
+        root = value
+        
+# print root.introduction()
+depth = 0
+depth = calucateDepth(root, dictionary, depth)
+print depth
