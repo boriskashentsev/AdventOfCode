a9fd0477b17d74dc4164c25f669d5e0314b66d72
Boris Kashentsev <boris.kashentsev@gmail.com>
2021-10-16 00:39:10 +0300

    Previous algorithm wasn't slow. It was missing increment for a loop. Solution for Year 2015 day 11

--- 2015/2015.11.py
+++ 2015/2015.11.py
@@ -1,6 +1,25 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+crippledAlphabet = "abcdefghjkmnpqrstuvwxyz"
 
-def nextPassword(currentPassword):
+def preparePassword(password):
+    '''Prepare password at the start to exclude letters O, I and L'''
+    numbers = [alphabet.index(char) for char in password]
+    exclude = 'oil'
+    excludeNumbers = [alphabet.index(char) for char in exclude]
+    index =0
+    while index < len(numbers):
+        if numbers[index] in excludeNumbers:
+            numbers[index] += 1
+            index += 1
+            while index < len(numbers):
+                numbers[index] = 0
+                index += 1
+        else:
+            index += 1
+    return ''.join([alphabet[number] for number in numbers])
+
+def nextPassword(currentPassword, alphabet):
+    '''Generate next password by increasing characters by one (using special alphabet)'''
     numbers = [alphabet.index(char) for char in currentPassword]
     index = len(numbers)-1
     increment = 1
@@ -12,18 +31,16 @@ def nextPassword(currentPassword):
     return ''.join([alphabet[number] for number in numbers])
 
 def checkForThreeLetters(password):
+    '''Check that there is ne increasing straight of at least three letters '''
     index = 0
     while index < len(password)-3:
         if alphabet.index(password[index]) + 1 == alphabet.index(password[index + 1]) and alphabet.index(password[index + 1]) + 1 == alphabet.index(password[index + 2]):
             return True
+        index += 1
     return False
 
-def checkNoOIL(password):
-    exclude = "oil"
-    checkValues = [False for char in exclude if char in password]
-    return len(checkValues) == 0
-
 def checkPairs(password):
+    '''Check that there are at least two different, non-overlapping pairs of letters'''
     index = 0
     numberOfPairs = 0
     while index < len(password)-1:
@@ -35,17 +52,30 @@ def checkPairs(password):
 
 
 def checkPassword(password):
-    return checkForThreeLetters(password) and checkNoOIL(password) and checkPairs(password)
+    '''Wrapper-checker'''
+    return checkForThreeLetters(password) and checkPairs(password)
 
 def nextPossiblePassword(password):
-    newPassword = nextPassword(password)
+    '''Generate next possible password accodring to checks'''
+    newPassword = preparePassword(password)
+    newPassword = nextPassword(newPassword, crippledAlphabet)
     while not checkPassword(newPassword):
-        newPassword = nextPassword(newPassword)
+        newPassword = nextPassword(newPassword, crippledAlphabet)
     return newPassword
 
+# Test
+
 #inputs = ["abcdefgh", "ghijklmn"]
-inputs = ['vzbxkghb']
+
+# Part 1
+
+#inputs = ['vzbxkghb']
+
+#Part 2
+
+inputs = ['vzbxxyzz']
 
 for input in inputs:
     nextPasswordString = nextPossiblePassword(input)
+    #nextPasswordString = preparePassword(input)
     print(input, '->', nextPasswordString)
