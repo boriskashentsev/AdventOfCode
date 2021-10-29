e9d65cddfc8054f54ce9530a316cb6bb61169aa6
Boris Kashentsev <boris.kashentsev@gmail.com>
2021-10-29 10:24:37 +0300

    Year 2015 day 13 both parts

--- 2015/2015.13.py
+++ 2015/2015.13.py
@@ -1,10 +1,10 @@
 def dealWithAPerson(person, people):
-    '''Add city to the disctionary if it doesn't exist already'''
+    '''Add person to the disctionary if s/he doesn't exist already'''
     if person not in people.keys():
         people[person] = len(people.keys())
 
 def dealWithHappiness (person1Index, person2Index, gainOrLose, number, happiness):
-    '''Add distance between the cities to the matrix'''
+    '''Add happiness value between two people to the matrix'''
     maxIndex = max(person1Index, person2Index)
     if maxIndex >= len(happiness):
         for i in range(len(happiness)):
@@ -13,14 +13,44 @@ def dealWithHappiness (person1Index, person2Index, gainOrLose, number, happiness
     happiness[person1Index][person2Index] = int(number) if gainOrLose == "gain" else -1 * int(number)
 
 def printMatrix(matrix):
+    '''Helpful matrix printer function'''
     for i in range(len(matrix)):
         print(matrix[i])
 
-filename = "2015/13.test.input"
+def calculateHappiness(order, happiness):
+    '''Calculate cummulative happiness around the table'''
+    joy = 0
+    trueOrder = [0] + order
+    for i in range(len(trueOrder)):
+        joy += happiness[trueOrder[i]][trueOrder[(i+1)%len(trueOrder)]] + happiness[trueOrder[i]][trueOrder[(i-1)%len(trueOrder)]]
+    return joy
+
+def generateNewOrderAndCountHappiness(leftover, result, happiness, maxValue):
+    if len(leftover) == 0:
+        joy = calculateHappiness(result, happiness)
+        maxReturn = joy if (maxValue < 0 or maxValue < joy) else maxValue
+        return maxReturn
+    
+    for i in range(len(leftover)):
+        value = leftover.pop(i)
+        result.append(value)
+        maxValue = generateNewOrderAndCountHappiness(leftover, result, happiness, maxValue)
+        leftover.insert(i, value)
+        result.pop()
+    return maxValue
+
+def findMaxHappiness(happiness):
+    indexes = list(range(1, len(happiness)))
+    result = generateNewOrderAndCountHappiness(indexes, [], happiness, -1)
+    return result
+
+filename = "2015/13.input"
 
 f = open(filename, "r")
 
 input = f.read().split("\n")
+
+# Part 1
 
 people = {}
 happiness  = [[0]]
@@ -31,5 +61,27 @@ for line in input:
     dealWithAPerson(parts[10][0:len(parts[10])-1], people)
     dealWithHappiness(people[parts[0]], people[parts[10][0:len(parts[10])-1]], parts[2] , parts[3], happiness)
 
+maxvalue = findMaxHappiness(happiness)
 
-printMatrix(happiness)
+print(maxvalue)
+
+# Part 2
+
+you = 'You'
+
+def dealWithYou(people):
+    '''Adding you to the table'''
+    dealWithAPerson(you, people)
+
+def dealWithYourHappiness(people, happiness):
+    '''Adding your joy values to the matrix'''
+    for i in range(len(people)):
+        dealWithHappiness(people[you], i, 'gain', '0', happiness)
+        dealWithHappiness(i, people[you], 'gain', '0', happiness)
+    return
+
+dealWithYou(people)
+dealWithYourHappiness(people, happiness)
+
+maxvalue = findMaxHappiness(happiness)
+print(maxvalue)
