e6563149b4d3d04a7695ac06da4ffc72f8cb2428
Boris Kashentsev <boris.kashentsev@gmail.com>
2021-12-03 07:53:35 +0200

    Continue developing Day 15 Year 2015

--- 2015/2015.15.py
+++ 2015/2015.15.py
@@ -1,3 +1,21 @@
+def countCookieScore(ingredients, components, isPart1):
+    capacity = 0
+    durability = 0
+    flavor = 0
+    texture = 0
+    calories = 0
+    ingredients_keys = list(ingredients.keys())
+    for i in range(len(ingredients_keys)):
+        capacity += components[i]*ingredients[ingredients_keys[i]]['capacity']
+        durability += components[i]*ingredients[ingredients_keys[i]]['durability']
+        flavor += components[i]*ingredients[ingredients_keys[i]]['flavor']
+        texture += components[i]*ingredients[ingredients_keys[i]]['texture']
+        calories += components[i]*ingredients[ingredients_keys[i]]['calories']
+    
+    if isPart1:
+        return capacity*durability*flavor*texture
+    return -1
+
 filename = "2015/15.test.input"
 
 f = open(filename, "r")
@@ -15,3 +33,7 @@ for line in input:
                                 'texture': int(parts[8][:-1]),
                                 'calories': int(parts[10])}
 
+components = [0]*len(input)
+components[0] = 100
+
+print(countCookieScore(ingredients, components, True))
