e99fc65bd1458b929cb84ef1c48bf1a3242b84a5
Boris Kashentsev <boris.kashentsev@gmail.com>
2023-12-04 10:07:39 +0200

    Part 2 Day 3 Year 2023

--- 2023/2023.03.py
+++ 2023/2023.03.py
@@ -47,4 +47,41 @@ for i in range(len(lines)):
 
 print('Part 1:', result)
 
+# Part 2
+
+dx = [0, 1, -1, 1,  0,  1, -1, -1]
+dy = [1, 1,  1, 0, -1, -1, -1,  0]
+
+result = 0
+
+for i in range(len(lines)):
+  line = lines[i]
+  for j in range(len(line)):
+    if line[j] == '*':
+      di = 0
+      numbersForMultiplication = []
+      while di < len(dx):
+        y = i + dy[di]
+        x = j + dx[di]
+        isNumber = False
+        if y in range(len(lines)) and x in range(len(lines[y])):
+          if lines[y][x] in numbers:
+            isNumber = True
+            # find start of the number
+            newJ = x
+            while newJ >= 0 and lines[y][newJ] in numbers:
+              newJ -= 1
+            newJ += 1
+            number = 0
+            while newJ < len(lines[y]) and lines[y][newJ] in numbers:
+              number = number * 10 + int(lines[y][newJ])
+              newJ += 1
+            numbersForMultiplication.append(number)
+        if isNumber and di % 4 == 0:
+          di += 2
+        di += 1
+      if len(numbersForMultiplication) == 2:
+        result += numbersForMultiplication[0] * numbersForMultiplication[1]
+
+print("Part 2:", result)
 
